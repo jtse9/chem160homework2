c8fafdd5d547409c2ef3424bcb8040b14f2e4d8e
jtse9 <54727553+jtse9@users.noreply.github.com>
2019-09-14 13:06:14 -0700

    Add files via upload

--- dice.py
+++ dice.py
@@ -1,17 +1,17 @@
 from random import choices
 ntrials = 10000
-player1wins = 0
+player2wins = 0
 for i in range(ntrials):
     total1 = 0
     total2 = 0
     dice1 = choices(range(1, 7), k=2)
     total1 = total1 + dice1[0] + dice1[1]
     if dice1[0] == dice1[1]:
-        player1wins += 1
+        player2wins += 1
         continue
     dice2 = choices(range(1, 7), k=3)
     dice2.sort(reverse=True)
     total2 = total2 + dice2[0] + dice2[1]
     if total2 <= total1:
-        player1wins +=1
-print("player1wins=", player1wins/ntrials)
+        player2wins +=1
+print("player1wins=", 1-player2wins/ntrials)
